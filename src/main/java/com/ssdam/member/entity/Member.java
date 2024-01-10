45b945f4e0240ea574e74150aae4abbc5f02f229
JUNHA CHOI <choijh0309@naver.com>
2024-01-10 23:35:32 +0900

    update 다시 태어났어요

---
+++ src/main/java/com/ssdam/member/entity/Member.java
@@ -0,0 +1,52 @@
+package com.ssdam.member.entity;
+
+import com.ssdam.audit.Auditable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Member extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long memberId;
+
+    @Column(nullable = false, updatable = false, unique = true)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Column(length = 100, nullable = false)
+    private String nickname;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(length = 10, nullable = false)
+    private MemberStatus memberStatus = MemberStatus.MEMBER_ACTIVE;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+    public Member(String email) {
+        this.email = email;
+    }
+
+    public enum MemberStatus {
+        MEMBER_ACTIVE("활동중"),
+        MEMBER_SLEEP("휴면 상태"),
+        MEMBER_QUIT("탈퇴 상태");
+
+        @Getter
+        private String status;
+
+        MemberStatus(String status) {
+            this.status = status;
+        }
+    }
+}
